73562bcf1f1c116778ac8eee067d6f757674c25d
Adminl <Adminl@PARME42>
2018-01-11 10:40:56 +0100

    creation entite client

---
+++ ProjetHotel/ProjetHotel/AppGrandHotel.cs
@@ -0,0 +1,35 @@
+﻿using Outils.TConsole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetHotel
+{
+    public class AppGrandHotel:ConsoleApplication
+    {
+        private static AppGrandHotel _instance;
+
+        public static AppGrandHotel Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new AppGrandHotel();
+
+                return _instance;
+            }
+        }
+
+        public AppGrandHotel()
+        {
+            // Définition des options de menu à ajouter dans tous les menus de pages
+            MenuPage.DefaultOptions.Add(
+                new Option("a", "Accueil", () => _instance.NavigateHome()));
+
+            //MenuPage.DefaultOptions.Add(
+            //	new Option("p", "Page précédente", () => _instance.NavigateBack()));
+        }
+    }
+}
--- ProjetHotel/ProjetHotel/Entite.cs
+++ ProjetHotel/ProjetHotel/Entite.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,20 +12,53 @@ namespace ProjetHotel
     {
 
 
-
-
         class Client
         {
             public int Id { get; set; }
+            [MaxLength(4)]
+            public string Civilite { get; set; }
+            public string Nom { get; set; }
+            public string Prenom { get; set; }
+            public bool CarteFidelite { get; set; }
+            public string Societe { get; set; }
+            // Propriété de navigation
+            public virtual Address Address { get; set; }
+            public virtual List<Telephone> Telephones { get; set; }
+            public virtual List<Email> Emails { get; set; }
+        }
 
+        class Address
+        {
+            [Key]
+            [ForeignKey("Client")]
+            public int IdClient  { get; set; }
+            public string Rue { get; set; }
+            public string Complement { get; set; }
+            [MaxLength(5)]
+            public string CodePostal { get; set; }
+            public string Ville { get; set; }
+            // Propriété de navigation
+            public virtual Client Client { get; set; }
+        }
 
-
+        class Telephone
+        {
+            [MaxLength(12)]
+            public string Numero { get; set; }
+            [ForeignKey("Client")]
+            public int IdClient { get; set; }
+            public char CodeType { get; set; }
+            public bool Pro { get; set; }
 
         }
 
-
-
-
+        class Email
+        {
+            public string Adresse { get; set; }
+            [ForeignKey("Client")]
+            public int IdClient { get; set; }
+            public bool Pro { get; set; }
+        }
 
 
 
